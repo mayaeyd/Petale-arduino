b3e377c6106b5d5f6716379fbc8f410feaf056da
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 02:26:28 +0200

    arduino: Add WebSocket event handling system

--- plant_monitoring/websocket_handler.cpp
+++ plant_monitoring/websocket_handler.cpp
@@ -22,4 +22,26 @@ void WebSocketHandler::loop() {
     reconnectWebSocket();
 }
 
+void WebSocketHandler::sendMessage(const String& message) {
+    webSocket.sendTXT(message);
+}
 
+void WebSocketHandler::webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+    getInstance().webSocketConnected = (type == WStype_CONNECTED);
+    
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.println("Disconnected from WebSocket server");
+            break;
+        case WStype_CONNECTED:
+            Serial.println("Connected to WebSocket server");
+            break;
+        case WStype_TEXT:
+            getInstance().handleWebSocketMessage(payload, length);
+            break;
+        case WStype_ERROR:
+            Serial.println("WebSocket Error");
+            getInstance().webSocketConnected = false;
+            break;
+    }
+}
