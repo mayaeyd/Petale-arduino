47bf4543eb398589e8fec53831c43e0db1b47c33
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 02:39:35 +0200

    arduino: Add WiFi connection management

--- plant_monitoring/plant_monitoring.ino
+++ plant_monitoring/plant_monitoring.ino
@@ -10,6 +10,16 @@ void setup() {
     
     sensors.begin();
     Watering::getInstance().begin();
+    
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to WiFi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 
     WebSocketHandler::getInstance().begin();
 }
