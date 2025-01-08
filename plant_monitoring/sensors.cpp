6aca5a8401f72502087ee90212a5fe6167711e06
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 02:18:39 +0200

    arduino: Add sesnor data reading and sending functionality

--- plant_monitoring/sensors.cpp
+++ plant_monitoring/sensors.cpp
@@ -10,3 +10,17 @@ void Sensors::begin() {
     delay(2000);
 }
 
+void Sensors::readAndSendData() {
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    int soilMoisture = map(analogRead(SOIL_MOISTURE_PIN), 0, 1023, 100, 0);
+    
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+    Serial.print("Soil Moisture: ");
+    Serial.println(soilMoisture);
+
+    
+}
