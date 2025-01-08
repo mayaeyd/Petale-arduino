e3fbdc14b261a9ac20b9c44da64f9e9b9d459bcf
Maya Eid <mayabaseleid@gmail.com>
2025-01-08 02:10:13 +0200

    arduino: Define pin assignments in config template file

---
+++ plant_monitoring/config.template.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <ESP8266WiFi.h>
+
+// Pin Definitions
+#define DHTPIN D2
+#define SOIL_MOISTURE_PIN A0
+#define PUMP_RELAY_PIN D1
+#define DHTTYPE DHT11
+
---
+++ plant_monitoring/sensors.h
@@ -0,0 +1,17 @@
+// sensors.h
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include <DHT.h>
+#include "config.h"
+
+class Sensors {
+public:
+    Sensors();
+    void begin();
+    void readAndSendData();
+private:
+    DHT dht;
+};
+
+#endif
